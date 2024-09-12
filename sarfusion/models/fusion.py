bfc491f332f10f948fdddac5db994dbbe491051c
Pasquale De Marinis <pasquale199876@hotmail.it>
2024-09-12 10:02:31 +0200

    new params

--- sarfusion/models/fusion.py
+++ sarfusion/models/fusion.py
@@ -5,12 +5,15 @@ from ultralytics.nn.modules import Conv
 
 
 class OptionalConv(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True, use_embedding=True):
         super(OptionalConv, self).__init__()
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride=stride, padding=padding, dilation=dilation, groups=groups, bias=bias)
         self.bn = nn.BatchNorm2d(out_channels)
         self.act = nn.LeakyReLU(0.1, inplace=True)
-        self.embedding = nn.Embedding(1, out_channels)
+        if use_embedding:
+            self.embedding = nn.Embedding(1, out_channels)
+        else:
+            self.embedding = None
 
     def forward(self, x, shape):
         if x is None:
@@ -19,6 +22,8 @@ class OptionalConv(nn.Module):
             # Conv reduction formula
             H = (H + 2 * self.conv.padding[0] - self.conv.dilation[0] * (self.conv.kernel_size[0] - 1) - 1) // self.conv.stride[0] + 1
             W = (W + 2 * self.conv.padding[1] - self.conv.dilation[1] * (self.conv.kernel_size[1] - 1) - 1) // self.conv.stride[1] + 1
+            if self.embedding is None:
+                return torch.zeros(B, self.conv.out_channels, H, W, device=device)
             return self.embedding(torch.tensor([0], device=device)).view(1, -1, 1, 1).expand(B, -1, H, W)
         x = self.conv(x)
         x = self.bn(x)
@@ -29,10 +34,10 @@ class OptionalConv(nn.Module):
 class FusionConv(nn.Module):
     IGNORE_VALUE = 114
     
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=None, dilation=1, groups=1, bias=True):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=None, dilation=1, groups=1, bias=True, use_embedding=True):
         super(FusionConv, self).__init__()
-        self.optional_rgb = OptionalConv(3, out_channels, kernel_size, stride=1, padding=1, dilation=dilation, groups=groups, bias=bias)
-        self.optional_ir = OptionalConv(1, out_channels, kernel_size, stride=1, padding=1, dilation=dilation, groups=groups, bias=bias)
+        self.optional_rgb = OptionalConv(3, out_channels, kernel_size, stride=1, padding=1, dilation=dilation, groups=groups, bias=bias, use_embedding=use_embedding)
+        self.optional_ir = OptionalConv(1, out_channels, kernel_size, stride=1, padding=1, dilation=dilation, groups=groups, bias=bias, use_embedding=use_embedding)
         assert in_channels == 4, "in_channels must be 4"
         
         self.conv = Conv(out_channels * 2, out_channels, kernel_size, s=stride, p=padding, d=dilation, g=groups)
